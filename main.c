ab7b92ed4f6ec40c669fa9d0941dc35caa6e007a
salaesteve2 <salamanca.esteve@gmail.com>
2022-07-28 19:43:04 +0200

    Updated

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: salustianosalamanca <salustianosalamanc    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 17:15:38 by salustianos       #+#    #+#             */
-/*   Updated: 2022/07/28 17:29:53 by salustianos      ###   ########.fr       */
+/*   Updated: 2022/07/28 19:42:20 by salustianos      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,33 @@ int ft_comprobar_salida(char *s)
 	return (0);
 }
 
+void	ft_pwd()
+{
+	char *buf;
+
+	buf = getcwd(NULL, 0);
+	printf("%s\n", buf);
+}
+
+void	ft_comprobar_comando(char *s)
+{
+	int x;
+
+	x = 0;
+	while (s[x])
+	{
+		if (s[x] == 'p')
+		{
+			if (s[x + 1] == 'w')
+			{
+				if (s[x + 2] == 'd' && s[x + 3] == '\0')
+					ft_pwd();
+			}
+		}
+		x++;
+	}
+}
+
 int main(void)
 {
 	int x;
@@ -47,6 +74,7 @@ int main(void)
 		printf("%s\n", texto);
 		if (ft_comprobar_salida(texto) == 1)
 			exit(0);
+		ft_comprobar_comando(texto);
 	}
 	return (0);
 }
