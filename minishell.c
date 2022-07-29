c37da75d3cb72c2a6afee25d3619fb8974755e53
salaesteve2 <salamanca.esteve@gmail.com>
2022-07-29 19:59:51 +0200

    Update

--- minishell.c
+++ minishell.c
@@ -6,14 +6,14 @@
 /*   By: salustianosalamanca <salustianosalamanc    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 17:16:04 by salustianos       #+#    #+#             */
-/*   Updated: 2022/07/29 19:22:29 by salustianos      ###   ########.fr       */
+/*   Updated: 2022/07/29 19:57:54 by salustianos      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // *! Necesito estructura para saber si tiene argumentos y cuales son
-// *! OBTENER PRIMERO EL ENVIRONMENT
+// *! Necesito primero obtener las variables de entorno
 void	ft_pwd()
 {
 	char *buf;
@@ -28,10 +28,23 @@ void	ft_pwd()
 	else
 		printf("pwd: Demasiados argumentos\n");
 }
-
+// *! Hay que modificar el codigo por las variables de entorno
+// *! Ver si funciona con directorios sin permiso
 void	ft_cd()
 {
-	
+	char *argumento;
+
+	argumento = NULL; // ? Provisional, esto lo da la estructura
+	if (!argumento)
+		chdir(getenv("HOME"));
+	else if (ft_strncmp(argumento, "..", 3) == 0)
+		chdir("..");
+	else
+	{
+		if (chdir(argumento) != 0)
+			printf("cd: No existe el directorio\n");
+	}
+		
 }
 
 void	ft_env()
