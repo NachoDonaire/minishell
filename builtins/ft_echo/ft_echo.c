05ef152df3fc35812e88c8bf77b55d4c6d0a4c1b
salaesteve2 <salamanca.esteve@gmail.com>
2022-08-04 15:27:41 +0200

    Update

--- builtins/ft_echo/ft_echo.c
+++ builtins/ft_echo/ft_echo.c
@@ -6,53 +6,64 @@
 /*   By: salustianosalamanca <salustianosalamanc    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 13:10:10 by sasalama          #+#    #+#             */
-/*   Updated: 2022/08/04 14:21:18 by salustianos      ###   ########.fr       */
+/*   Updated: 2022/08/04 15:18:08 by salustianos      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-static void	ft_imprimir(char **string, int x, char **env)
+static void	ft_imprimir(char **s, int x, char **env, int z)
 {
 	int		y;
 	int		a;
 	int		b;
+	int		c;
 
-	if (ft_dollar(string[x]) == 0)
-		printf("%s", string[x]);
+	c = ft_strlen(s[2]) - 1;
+	if (ft_dollar(s[x]) == 0)
+		printf("%s", s[x]);
 	else
 	{
+		if (s[x][0] == '\"' && s[x][z] == '\"' && s[x + 1] && s[0][0] != 39 &&
+			s[0][0] == 34 && s[2][c] != 34) // El 2 es el nº de argumentos
+			printf("\"");
 		y = -1;
-		while (string[x][++y])
+		while (s[x][++y])
 		{
-			a = ft_comillas_simples(string[x]);
-			b = ft_comillas_dobles(string[x]);
-			if (string[x][y] != '$' && string[x][y] != 39 && string[x][y] != 34)
-				printf("%c", string[x][y]);
-			else if (string[x][y] == '$' && a == 0 && b == 0)
+			a = ft_comillas_simples(s[x]);
+			b = ft_comillas_dobles(s[x]);
+			if (s[x][y] != '$' && s[x][y] != 39 && s[x][y] != 34)
+				printf("%c", s[x][y]);
+			else if (s[x][y] == '$' && a == 0 && b == 0)
 			{
-				ft_imprimir_variable(string, x, y, env);
+				ft_imprimir_variable(s, x, y, env);
 				break ;
 			}
-			else if (string[x][y] != 39 && string[x][y] != 34)
-				printf("%c", string[x][y]);
+			else if (s[x][y] != 39 && s[x][y] != 34)
+				printf("%c", s[x][y]);
 		}
+		if (s[x][0] == '\"' && s[x][z] == '\"' && s[x + 1] && s[0][0] != 39 &&
+			s[0][0] == 34 && s[2][c] != 34) // El 2 es el nº de argumentos
+			printf("\"");
 	}
 }
 
 static	void	ft_imprimir_comillas(char **array, int x, char **e, int *c)
 {
-	if (ft_imprimir_comilla_simple(array[x]) == 0)
+	int	z;
+
+	z = ft_strlen(array[x]) - 1;
+	if (ft_imprimir_comilla_simple(array[x]) == 0 && array[x][z] != 34)
 	{
 		c[0] = 1;
 		printf("'");
 	}
-	if (ft_imprimir_comilla_doble(array[x]) == 0)
+	if (ft_imprimir_comilla_doble(array[x]) == 0 && array[x][z] != 39)
 	{
 		c[1] = 1;
 		printf("\"");
 	}
-	ft_imprimir(array, x, e);
+	ft_imprimir(array, x, e, z);
 	if (array[x + 1])
 		printf(" ");
 }
@@ -90,7 +101,7 @@ void	ft_echo(char **env) // *! MODIFICAR para comillas simples
 
 	x = 0;
 	new_line = 1;
-	nb_argumentos = ft_split("'-nnnnnnt -nnnnn$TERM'", ' ');
+	nb_argumentos = ft_split("\"-nnnnnn \"t\" -nnnnn$TERM\"", ' ');
 	if (nb_argumentos)
 	{
 		new_line = ft_comprobar_saltos(nb_argumentos, x, env);
